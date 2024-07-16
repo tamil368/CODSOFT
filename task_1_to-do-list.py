466614b65a53eb97340d3ec6d7a475526af68b1e
tamil368 <tamilaathi2020@gmail.com>
2024-07-16 14:23:55 +0530

    updated gui model

--- task_1_to-do-list.py
+++ task_1_to-do-list.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+from tkinter import messagebox, Toplevel
+
 class Todo:
     def __init__(self):
         self.Tasks = []
@@ -6,7 +9,8 @@ class Todo:
         self.Tasks.append(task)
 
     def remove_task(self, task):
-        self.Tasks.remove(task)
+        if task in self.Tasks:
+            self.Tasks.remove(task)
 
     def update_task(self, old_task, new_task):
         if old_task in self.Tasks:
@@ -14,46 +18,98 @@ class Todo:
             self.Tasks[index] = new_task
 
     def display_tasks(self):
-        print("TO-Do list")
-        for task in self.Tasks:
-            print(task)
+        tasks = "\n".join(self.Tasks)
+        return tasks
 
-def add_task(todo):
-    task = input("Enter task to add: ")
-    todo.add_task(task)
+class TodoApp:
+    def __init__(self, root):
+        self.todo = Todo()
+        self.root = root
+        self.root.title("ToDo List App")
 
-def remove_task(todo):
-    task = input("Enter task to remove: ")
-    todo.remove_task(task)
+        self.task_label = tk.Label(self.root, text="", justify=tk.LEFT)
+        self.task_label.pack()
 
-def update_task(todo):
-    old_task = input("Enter task to update: ")
-    new_task = input("Enter new task: ")
-    todo.update_task(old_task, new_task)
+        self.entry = tk.Entry(self.root)
+        self.entry.pack()
 
-def display_tasks(todo):
-    todo.display_tasks()
+        self.add_button = tk.Button(root, text="Add Task", command=self.add_task)
+        self.add_button.pack()
 
-to_do = Todo()
+        self.remove_button = tk.Button(root, text="Remove Task", command=self.open_remove_window)
+        self.remove_button.pack()
 
-while True:
-    print("1. Add Task")
-    print("2. Remove Task")
-    print("3. Update Task")
-    print("4. Display Tasks")
-    print("5. Exit")
+        self.update_button = tk.Button(root, text="Update Task", command=self.open_update_window)
+        self.update_button.pack()
 
-    choice = input("Enter your choice: ")
+        self.display_tasks()
 
-    if choice == '1':
-        add_task(to_do)
-    elif choice == '2':
-        remove_task(to_do)
-    elif choice == '3':
-        update_task(to_do)
-    elif choice == '4':
-        display_tasks(to_do)
-    elif choice == '5':
-        break
-    else:
-        print("Invalid choice. Please try again.")
+    def add_task(self):
+        task = self.entry.get()
+        if task:
+            self.todo.add_task(task)
+            self.entry.delete(0, tk.END)
+            self.display_tasks()
+
+    def open_remove_window(self):
+        self.remove_window = Toplevel(self.root)
+        self.remove_window.title("Remove Task")
+
+        self.remove_task_label = tk.Label(self.remove_window, text="Task Name")
+        self.remove_task_label.pack()
+        self.remove_task_entry = tk.Entry(self.remove_window)
+        self.remove_task_entry.pack()
+
+        self.remove_confirm_button = tk.Button(self.remove_window, text="Remove Task", command=self.remove_task)
+        self.remove_confirm_button.pack()
+
+    def remove_task(self):
+        task = self.remove_task_entry.get()
+        if task:
+            if task in self.todo.Tasks:
+                self.todo.remove_task(task)
+                self.display_tasks()
+                self.remove_window.destroy()
+            else:
+                messagebox.showerror("Error", "Task not found")
+        else:
+            messagebox.showerror("Error", "Task name cannot be empty")
+
+    def open_update_window(self):
+        self.update_window = Toplevel(self.root)
+        self.update_window.title("Update Task")
+
+        self.old_task_label = tk.Label(self.update_window, text="Old Task Name")
+        self.old_task_label.pack()
+        self.old_task_entry = tk.Entry(self.update_window)
+        self.old_task_entry.pack()
+
+        self.new_task_label = tk.Label(self.update_window, text="New Task Name")
+        self.new_task_label.pack()
+        self.new_task_entry = tk.Entry(self.update_window)
+        self.new_task_entry.pack()
+
+        self.update_confirm_button = tk.Button(self.update_window, text="Update Task", command=self.update_task)
+        self.update_confirm_button.pack()
+
+    def update_task(self):
+        old_task = self.old_task_entry.get()
+        new_task = self.new_task_entry.get()
+        if old_task and new_task:
+            if old_task in self.todo.Tasks:
+                self.todo.update_task(old_task, new_task)
+                self.display_tasks()
+                self.update_window.destroy()
+            else:
+                messagebox.showerror("Error", "Old task not found")
+        else:
+            messagebox.showerror("Error", "Both fields must be filled")
+
+    def display_tasks(self):
+        tasks = self.todo.display_tasks()
+        self.task_label.config(text=tasks)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TodoApp(root)
+    root.mainloop()
